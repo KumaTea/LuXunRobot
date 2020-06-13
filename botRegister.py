f730ee954c55929eaac8c7449a92daea11c48846
KumaTea <36222458+KumaTea@users.noreply.github.com>
2020-06-13 15:12:14 +0800

    Add corpus, apply and adjust

--- botRegister.py
+++ botRegister.py
@@ -9,7 +9,7 @@ def start_cron():
     # scheduler.add_job(tweet, 'cron', hour=2, minute=0)
     scheduler.add_job(morning, 'cron', hour=7, minute=30)
     scheduler.add_job(night, 'cron', hour=2, minute=30)
-    scheduler.add_job(process_mention, 'cron', minute='*')
+    scheduler.add_job(process_mention, 'cron', minute='*/5')
 
 
 def handlers():
